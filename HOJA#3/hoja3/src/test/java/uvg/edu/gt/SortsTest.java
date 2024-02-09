b4622e8317fe004310dceaedf195f4e4964e2480
SebastianUVG <gar22291@uvg.edu.gt>
2024-02-08 22:39:50 -0600

    Intentando hacer las test
    no se pudieron realizar las test

--- HOJA#3/hoja3/src/test/java/uvg/edu/gt/SortsTest.java
+++ HOJA#3/hoja3/src/test/java/uvg/edu/gt/SortsTest.java
@@ -1,5 +1,6 @@
 package uvg.edu.gt;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,6 +16,18 @@ public class SortsTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Sorts sort = new Sorts();
+        Integer[] input = new Integer[]{5, 4, 3, 2, 1};
+
+        // Crear una copia ordenada previamente del arreglo de entrada
+        Integer[] expected = new Integer[]{1, 2, 3, 4, 5};
+
+        // Llamar a la función bubbleSort
+        
+
+        // Verificar si el arreglo resultante es igual al arreglo ordenado previamente
+        assertArrayEquals(sort.bubbleSort(input), expected;
     }
 }
+
+// Trate de hacer las test, pero por la forma en que lo realice no pude.
