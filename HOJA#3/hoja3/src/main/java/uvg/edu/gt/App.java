89c38c1d43d9c2d437b45f9f4f9e919429c1bb39
SebastianUVG <gar22291@uvg.edu.gt>
2024-02-08 20:50:57 -0600

    Agregar los ultimos sorts
    Se agregaron los ultimos Sorts al programa

--- HOJA#3/hoja3/src/main/java/uvg/edu/gt/App.java
+++ HOJA#3/hoja3/src/main/java/uvg/edu/gt/App.java
@@ -8,13 +8,18 @@ public class App
 {
     public static void main( String[] args )
     {     
-        for (int i = 10 ; i<=20 ; i++){
+        for (int i = 10 ; i<=15 ; i++){
             Integer[] arreglo = new Integer[i];
             for (int x = 0 ; x<i ; x++){
                 arreglo[x] = (int)Math.floor(Math.random()*3000);
             }
+
+            System.out.println("Arreglo desordenado de tamaño " + i +": " + java.util.Arrays.toString(arreglo));
             //Sorts.bubbleSort(arreglo);
-            Sorts.mergeSort(arreglo);
+            //Sorts.mergeSort(arreglo);
+            //Sorts.quickSort(arreglo, 0, arreglo.length-1);
+            //Sorts.mergeSort(arreglo);
+            Sorts.radixSort(arreglo);
             System.out.println("Arreglo de tamaño " + i + ": " + java.util.Arrays.toString(arreglo));
         }
     }
--- HOJA#3/hoja3/src/main/java/uvg/edu/gt/Sorts.java
+++ HOJA#3/hoja3/src/main/java/uvg/edu/gt/Sorts.java
@@ -107,18 +107,80 @@ public class Sorts {
         cambio(array, i + 1, high);
         return i + 1;
     }
+
 /**
- * Funcion que hace los cambios
+ * GNOME SORT
  * @param <T>
  * @param array
- * @param i
- * @param j
  */
+
+    public static <T extends Comparable<T>> void gnomeSort(T[] array) {
+        int index = 0;
+
+        while (index < array.length) {
+            if (index == 0 || array[index - 1].compareTo(array[index]) <= 0) {
+                index++;
+            } else {
+                cambio(array, index, index - 1);
+                index--;
+            }
+        }
+    }
+
+    /**
+     * Funcion que hace los cambios
+     * @param <T>
+     * @param array
+     * @param i
+     * @param j
+     */
     private static <T> void cambio(T[] array, int i, int j) {
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
+
+
+
+    public static <T extends Comparable<T>> void radixSort(T[] array) {
+        if (array.length == 0) {
+            return;
+        }
+
+        // Encontrar el elemento máximo para determinar el número de dígitos
+        T max = Arrays.stream(array).max(Comparable::compareTo).get();
+        int exp = 1;
+
+        while (((Integer) max / exp) > 0) {
+            countingSort(array, exp);
+            exp *= 10;
+        }
+    }
+
+
+    private static <T extends Comparable<T>> void countingSort(T[] array, int exp) {
+        int n = array.length;
+        T[] output = Arrays.copyOf(array, n);
+        int[] count = new int[10];
+
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[((Integer) (array[i]) / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[((Integer) (array[i]) / exp) % 10] - 1] = array[i];
+            count[((Integer) (array[i]) / exp) % 10]--;
+        }
+
+        System.arraycopy(output, 0, array, 0, n);
+    }
+
 }
 
 
